bdaf35704a36d14c6fbe3fb14ef6416c1bfdd8ac
ekrambd <ekramhossainekram28@gmail.com>
2026-04-17 10:57:07 +0600

    leetcode 217 problem solved

---
+++ array/leetcode_217.js
@@ -0,0 +1,14 @@
+//217. Contains Duplicate
+
+var containsDuplicate = function(nums){
+    let map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])){
+            return true;
+        }
+        map.set(nums[i],i);
+    }
+    return false;
+};
+console.log(containsDuplicate([1,2,3,1]));
+console.log(containsDuplicate([1,2,3,4]));
