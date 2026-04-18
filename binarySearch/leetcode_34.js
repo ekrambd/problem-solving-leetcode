178837c6a3a94d43a2b0c6b9f60a6408008eba79
ekrambd <ekramhossainekram28@gmail.com>
2026-04-18 21:40:33 +0600

    leetcode 34 solved

---
+++ binarySearch/leetcode_34.js
@@ -0,0 +1,44 @@
+//34. Find First and Last Position of Element in Sorted Array
+
+var searchRange = function(nums, target) {
+    let findFirst = () => {
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
+        while(left <= right){
+            let mid = Math.floor((left + right) / 2);
+            if(nums[mid] === target){
+                ans = mid;
+                right = mid - 1;
+            }
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return ans;
+    };
+
+    let findSecond = () => {
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
+        while(left <= right){
+            let mid = Math.floor((left + right) / 2);
+            if(nums[mid] === target){
+                ans = mid;
+                left = mid + 1;
+            }
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return ans;
+    };
+    return [findFirst(),findSecond()];
+};
+
+console.log(searchRange([5,7,7,8,8,10],8))
